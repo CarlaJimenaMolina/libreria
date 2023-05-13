f08303aaa8bbd0ed32c7335655193bf6e9873414
Carla Jimena Molina <carlajimenamolina@gmail.com>
2023-05-13 11:59:45 -0300

    avances

--- src/main/java/com/cjm/biblioteca/controladores/LibroControlador.java
+++ src/main/java/com/cjm/biblioteca/controladores/LibroControlador.java
@@ -1,12 +1,18 @@
 package com.cjm.biblioteca.controladores;
 
+import com.cjm.biblioteca.excepciones.BibliotecaException;
 import com.cjm.biblioteca.servicios.AutorServicio;
 import com.cjm.biblioteca.servicios.EditorialServicio;
 import com.cjm.biblioteca.servicios.LibroServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/libro")
@@ -22,6 +28,19 @@ public class LibroControlador {
     @GetMapping("/registrar")
     public String registrar(){
         return "libro_form.html";
+    }
+
+    @PostMapping("/registro")
+    public String registro(@RequestParam(required = false) Long isbn, @RequestParam String titulo,
+                           @RequestParam(required = false) Integer ejemplares,
+                           @RequestParam String idAutor, @RequestParam String idEditorial){
+        try {
+            libroServicio.crearLibro(isbn, titulo, ejemplares, idAutor,idEditorial );
+        } catch (BibliotecaException e) {
+            Logger.getLogger(AutorControlador.class.getName()).log(Level.SEVERE, null, e);
+            return "libro_form.html";
+        }
+        return "index.html";
     }
 
 }
